8057bfc63748730e6f0a4c03e378a3c42eb8357a
Kum yew <lokekumyew@gmail.com>
2025-02-02 12:41:38 +0800

    Updated skills

--- src/components/About.js
+++ src/components/About.js
@@ -19,26 +19,65 @@ const About = () => {
                     </p>
             </div>
             <div className="about-subsection">   
-                <h4>Skills</h4>
+                <h4>Technical Skills</h4>
                 <ul>
+                    {/* <!-- Languages --> */}
+                    <li class="category">Languages</li>
                     <li>JavaScript</li>
+                    <li>Python</li>
+                    <li>PHP</li>
+
+                    {/* <!-- Frontend --> */}
+                    <li class="category">Frontend</li>
+                    <li>React</li>
+                    <li>React Native</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+
+                    {/* <!-- Backend --> */}
+                    <li class="category">Backend</li>
+                    <li>Node.js</li>
+                    <li>Express.js</li>
+
+                    {/* <!-- Databases --> */}
+                    <li class="category">Databases</li>
+                    <li>MongoDB</li>
+                    <li>PostgreSQL</li>
+                    <li>MySQL</li>
+
+                    {/* <!-- DevOps & Deployment --> */}
+                    <li class="category">Tools</li>
+                    <li>GitHub</li>
+                    <li>Vercel</li>
+                    <li>Docker</li>
+                    <li>CI/CD</li>
+
+                    {/* <!-- Data & Analysis --> */}
+                    <li class="category">Other Skills</li>
+                    <li>RESTful APIs</li>
+                    <li>API Integration</li>
+                    <li>UI/UX Design</li>
+                    <li>Data Analysis</li>
+                    <li>Project Management</li>
+                    {/* <li>JavaScript</li>
                     <li>React</li>
                     <li>React Native</li>
                     <li>Node.js</li>
                     <li>HTML</li>
                     <li>CSS</li>
+                    <li>PHP</li>
                     <li>Python</li>
                     <li>MongoDB</li>
                     <li>PostgreSQL</li>
+                    <li>MySQL</li>
+                    <li>Express.js</li>
+                    <li>RESTful APIs</li>
                     <li>API Integration</li>
                     <li>UI/UX Design</li>
                     <li>Data Analysis</li>
                     <li>Project Management</li>
                     <li>GitHub</li>
-                    
-                    
-                    
-
+                    <li>Vercel</li> */}
                 </ul>
             </div>
         </section>
